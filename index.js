bb70af25debe441e51e260712b7cd05fe1a30ef7
rsz <rhenansz@hotmail.com>
2022-07-03 21:03:57 -0300

    update commit

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require('express');
-
-const { Product } = require('./models');
+require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.get('/', (_req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
 
-app.post('/product', async (req, res) => {
-  const { name, description } = req.body;
-
-  const product = await Product.create({ name, description });
-
-  return res.status(201).json(product);
-});
-
-app.listen(port, () => console.log(`Servidor online na porta ${port}`));
+app.listen(port, () => console.log(`Online: ${port}`));
